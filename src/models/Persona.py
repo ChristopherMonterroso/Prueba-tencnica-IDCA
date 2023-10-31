848db65f0ec9a711c52c46ac999e59e10bd6d9b2
Christopher Monterroso <chrismonc08@gmail.com>
2023-10-31 15:02:06 -0600

    se cargan plantillas para uso de la api

---
+++ src/models/Persona.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self, nombres, apellidos, fecha_nacimiento, genero, direccion, estado_civil, dpi):
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.fecha_nacimiento = fecha_nacimiento
+        self.genero = genero
+        self.direccion = direccion
+        self.estado_civil = estado_civil
+        self.dpi = dpi
